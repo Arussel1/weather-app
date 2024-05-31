5bc94c979f20685aa371580f349acc49a89d7601
Arussel <anhminh05102004@gmail.com>
2024-05-31 02:38:40 -0400

    Complete web styling

--- src/assets/displayData.js
+++ src/assets/displayData.js
@@ -4,8 +4,8 @@ import partlycloudy from './../images/partlycloudy.gif';
 import rainy from './../images/rainy.gif'; 
 import clear from './../images/clear.jpg';
 
-export default function displayData(info, celsius = true) {
-    const body = document.querySelector("body");
+export default function displayData(info, celsius) {
+    const content = document.querySelector(".content");
     const infoContainer = document.createElement('div');
 
     // Main info display
@@ -15,7 +15,8 @@ export default function displayData(info, celsius = true) {
     const currentText = document.createElement('p');
     const currentIcon = document.createElement('img');
     const currentTemp = document.createElement('p');
-    const minmaxTemp = document.createElement('p');
+    const maxTemp = document.createElement('p');
+    const minTemp = document.createElement('p');
     const feelsLikeTemp = document.createElement('p');
 
     const sideInfo = document.createElement("div");
@@ -24,51 +25,54 @@ export default function displayData(info, celsius = true) {
     const rainChance = document.createElement('p');
     const UVIndex = document.createElement('p');
 
-    location.textContent = `${info.location.city}, ${info.location.region}, ${info.location.country}`;
+    location.textContent = `${info.location.city}, ${info.location.country}`;
     time.textContent = `${info.location.localTime}`;
     currentText.textContent = `${info.currentInfo.conditionText}`;
     currentIcon.src = `${info.currentInfo.conditionIcon}`;
     if(celsius){
         currentTemp.textContent = `${info.currentInfo.currentTemp} °C`;
-        minmaxTemp.textContent = `Min: ${info.currentInfo.minTemp} °C Max: ${info.currentInfo.maxTemp} °C`;
+        maxTemp.innerHTML = `Max: ${info.currentInfo.maxTemp} °C`;
+        minTemp.textContent = `Min: ${info.currentInfo.minTemp} °C`;
         feelsLikeTemp.textContent = `Feels Like: ${info.currentInfo.currentFeelsLikeTemp} °C`;
     }else{
-        currentTemp.textContent = `${info.currentInfo.currentTemp * 9 / 5 + 32} °F`;
-        minmaxTemp.textContent = `Min: ${info.currentInfo.minTemp * 9 / 5 + 32 } °F Max: ${info.currentInfo.maxTemp * 9 / 5 + 32} °F`;
-        feelsLikeTemp.textContent = `Feels Like: ${info.currentInfo.currentFeelsLikeTemp * 9 / 5 + 32} °F`;
+        currentTemp.textContent = `${Math.round(info.currentInfo.currentTemp * 9 / 5 + 32 )} °F`;
+        maxTemp.textContent = `Max: ${Math.round(info.currentInfo.maxTemp * 9 / 5 + 32)} °F`;
+        minTemp.textContent = `Min: ${Math.round(info.currentInfo.minTemp * 9 / 5 + 32)} °F`;
+        feelsLikeTemp.textContent = `Feels Like: ${Math.round(info.currentInfo.currentFeelsLikeTemp * 9 / 5 + 32)} °F`;
     }
 
-    windSpeed.textContent = `Wind speed: ${info.moreInfo.windSpeed}`;
-    humidity.textContent = `Humidity: ${info.moreInfo.humidity}`;
-    rainChance.textContent = `Rain Chance: ${info.moreInfo.rainChance}`;
-    UVIndex.textContent = `UV: ${info.moreInfo.UVIndex}`;
+    windSpeed.textContent = `Wind speed: ${info.moreInfo.windSpeed} km/h`;
+    humidity.textContent = `Humidity: ${info.moreInfo.humidity}%`;
+    rainChance.textContent = `Rain Chance: ${info.moreInfo.rainChance}%`;
+    UVIndex.textContent = `UV Index: ${info.moreInfo.UVIndex}`;
 
     mainInfo.appendChild(location);
     mainInfo.appendChild(time);
     mainInfo.appendChild(currentText);
     mainInfo.appendChild(currentIcon);
     mainInfo.appendChild(currentTemp);
-    mainInfo.appendChild(minmaxTemp);
+    mainInfo.appendChild(maxTemp);
+    mainInfo.appendChild(minTemp);
     mainInfo.appendChild(feelsLikeTemp);
-    mainInfo.classList.add = "mainInfo";
+    mainInfo.classList.add("mainInfo");
 
     sideInfo.append(windSpeed);
     sideInfo.append(humidity);
     sideInfo.append(rainChance);
     sideInfo.append(UVIndex);
-    sideInfo.classList.add = "sideInfo";
+    sideInfo.classList.add("sideInfo");
 
     infoContainer.appendChild(mainInfo);
     infoContainer.appendChild(sideInfo);
+    infoContainer.classList.add("infoContainer");
 
-
-    body.appendChild(infoContainer);
+    content.appendChild(infoContainer);
 
     const conditionText = info.currentInfo.conditionText.toLowerCase().trim();
     setBackgroundImage(conditionText);
 
     const forecastContainer = forecastDisplay(info,celsius);
-    body.appendChild(forecastContainer);
+    content.appendChild(forecastContainer);
 }
 
 function setBackgroundImage(text){
@@ -104,21 +108,18 @@ function forecastDisplay(info, celsius){
         const maxTemp = document.createElement('p');
         const rainChance = document.createElement('p');
         const icon = document.createElement('img');
-
         date.textContent = `${element.date}`;
         icon.src = `${element.icon}`;
-        rainChance.textContent = `Rain chance: ${element.rainChance}`;
+        rainChance.textContent = `Rain chance: ${element.rainChance}%`;
 
-        if(celsius){
-            maxTemp.textContent = `${element.maxTemp} °C`;
-            minTemp.textContent = `${element.minTemp} °C`;
+        if(celsius == true){
+            maxTemp.textContent = `Max: ${element.maxTemp} °C`;
+            minTemp.textContent = `Min: ${element.minTemp} °C`;
         }else{
-            maxTemp.textContent = `${element.maxTemp * 9 / 5 + 32} °F`;
-            minTemp.textContent = `${element.minTemp * 9 / 5 + 32 } °F`;
+            maxTemp.textContent = `Max: ${Math.round(element.maxTemp * 9 / 5 + 32)} °F`;
+            minTemp.textContent = `Min: ${Math.round(element.minTemp * 9 / 5 + 32)} °F`;
         }
 
-        maxTemp.textContent = `${element.maxTemp}`;
-        minTemp.textContent = `${element.minTemp} °C`;
 
         forecast.appendChild(date);
         forecast.appendChild(icon);
@@ -127,6 +128,7 @@ function forecastDisplay(info, celsius){
         forecast.appendChild(minTemp);
 
         forecastContainer.appendChild(forecast);
+        forecastContainer.classList.add("forecastContainer");
     });
     return forecastContainer;
 }
--- src/index.js
+++ src/index.js
@@ -4,22 +4,29 @@ import processWeatherData from './assets/processData';
 import sunny from './images/sunny.jpg';
 import displayData from './assets/displayData';
 
+let celsius = true;
+const degree = document.querySelector('#degree')
 const body = document.querySelector("body");
+const content = document.querySelector(".content");
 const span = document.querySelector("span");
-const button = document.querySelector("button");
+const submit = document.querySelector("button[type=submit]");
 const input = document.querySelector("input");
 
 body.style.backgroundImage = `url(${sunny})`;
 body.style.backgroundSize = '100% auto';  
 
-button.addEventListener("click", (event) =>{
+
+
+submit.addEventListener("click", (event) =>{
     event.preventDefault();
     const city = input.value.trim();
     if(city){
         const data =  getWeatherData(city);
     data.then((data) => {
         const processedData = processWeatherData(data);
-        displayData(processedData);
+        content.innerHTML = '';
+        span.textContent = "";
+        displayData(processedData,celsius);
     }).catch((error) => {
         console.error('Error processing weather data:', error);
         span.textContent = "Cannot find the city";
@@ -30,3 +37,13 @@ button.addEventListener("click", (event) =>{
     
 })
 
+degree.addEventListener("click", () => {
+    celsius = !celsius;
+    degree.textContent = ' ';
+    if(celsius){
+        degree.textContent = "°C";
+        
+    }else{
+        degree.textContent = "°F";
+    }
+});
